63058fd2fd2ceea6145675709bbbff53b946a69b
jasontung <jasontung@dongweichengde-MacBook-Pro.local>
2017-10-27 21:24:28 +0800

    1. setup audio mixer

--- Assets/_Complete-Game/Scripts/Audio/MixLevels.cs
+++ Assets/_Complete-Game/Scripts/Audio/MixLevels.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Audio;
+namespace CompleteProject
+{
+    public class MixLevels : MonoBehaviour
+    {
 
-public class MixLevels : MonoBehaviour {
+        public AudioMixer masterMixer;
 
-	public AudioMixer masterMixer;
+        public void SetSfxLvl(float sfxLvl)
+        {
+            masterMixer.SetFloat("sfxVol", sfxLvl);
+        }
 
-	public void SetSfxLvl(float sfxLvl)
-	{
-		masterMixer.SetFloat("sfxVol", sfxLvl);
-	}
-
-	public void SetMusicLvl (float musicLvl)
-	{
-		masterMixer.SetFloat ("musicVol", musicLvl);
-	}
+        public void SetMusicLvl(float musicLvl)
+        {
+            masterMixer.SetFloat("musicVol", musicLvl);
+        }
+    }
 }
+
+
